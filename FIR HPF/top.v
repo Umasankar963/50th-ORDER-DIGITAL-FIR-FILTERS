dfc1b1014d2f294fde487cd4acbb17cc6fe0878f
Uma Sankar Gutta <105588869+Umasankar963@users.noreply.github.com>
2026-01-02 18:18:26 +0530

    Update top.v

--- FIR HPF/top.v
+++ FIR HPF/top.v
@@ -1,3 +1,5 @@
+
+
 `timescale 1ns / 1ps
 
 
@@ -13,15 +15,18 @@ module top (
     wire clk_out2;
     wire valid_o_t;
     wire valid_input_o_t;
-    
+
+	
 clk_wiz_0 instance_ila_clk
    (
     // Clock out ports
     .clk_out1(clk_out1),     // output clk_out1 100Mhz
     .clk_out2(clk_out2),     // output clk_out2 50Mhz
    // Clock in ports
-    .clk_in1(clk_i));
-    
+    .clk_in1(clk_i)
+   );
+
+	
     // ROM interface module supplies x_in
     rom_interface rom_inst (
         .clk(clk_out2),
@@ -30,6 +35,7 @@ clk_wiz_0 instance_ila_clk
         .valid_input_o(valid_input_o_t)
     );
 
+	
     // FIR filter with 51 taps
     fir_highpass_51 fir_inst (
         .clk(clk_out2),
@@ -42,8 +48,6 @@ clk_wiz_0 instance_ila_clk
     
 ila_0 instance_lpf (
 	.clk(clk_out1), // input wire clk
-
-
 	.probe0(rst_i), // input wire [0:0]  probe0  
 	.probe1(x_in_t), // input wire [15:0]  probe1 
 	.probe2(y_out_t), // input wire [38:0]  probe2
@@ -53,3 +57,4 @@ ila_0 instance_lpf (
 
 
 endmodule
+
