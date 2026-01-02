9edf6a4f2f1c4f82863286f01091a26804c9ed00
Uma Sankar Gutta <105588869+Umasankar963@users.noreply.github.com>
2026-01-02 18:04:10 +0530

    Update top.v

--- FIR BPF/top.v
+++ FIR BPF/top.v
@@ -1,5 +1,5 @@
-`timescale 1ns / 1ps
 
+`timescale 1ns / 1ps
 
 module top (
     input wire clk_i,
@@ -9,19 +9,23 @@ module top (
     
     wire signed [15:0] x_in_t;
     wire signed [38:0] y_out_t;
+	
     wire clk_out1;
     wire clk_out2;
     wire valid_o_t;
     wire valid_input_o_t;
-    
+
+	
 clk_wiz_0 instance_ila_clk
    (
     // Clock out ports
     .clk_out1(clk_out1),     // output clk_out1 100Mhz
     .clk_out2(clk_out2),     // output clk_out2 50Mhz
    // Clock in ports
-    .clk_in1(clk_i));
-    
+    .clk_in1(clk_i)
+   );
+
+	
     // ROM interface module supplies x_in
     rom_interface rom_inst (
         .clk(clk_out2),
@@ -30,6 +34,7 @@ clk_wiz_0 instance_ila_clk
         .valid_input_o(valid_input_o_t)
     );
 
+	
     // FIR filter with 51 taps
     fir_highpass_51 fir_inst (
         .clk(clk_out2),
